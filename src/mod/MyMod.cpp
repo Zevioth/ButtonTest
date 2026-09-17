e085c99165b0aa45abbe1195962f1b5d7ca2dc49
Zevioth <zevioth@gmail.com>
2026-09-17 18:43:55 +0800

    Update MyMod.cpp

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,128 +1,33 @@
-#include "mod/MyMod.h"
+#pragma once
 
-#include <filesystem>
+#include <optional>
 
+#include "mod/Config.h"
 #include <pl/Mod.hpp>
-#include <pl/ModMenu.hpp>
 
 namespace clange_me {
 
-ClangeMeMod &ClangeMeMod::instance() {
-    static ClangeMeMod instance;
-    return instance;
-}
+class ClangeMeMod {
+public:
+    static ClangeMeMod &instance();
 
-ClangeMeMod::ClangeMeMod()
-    : mSelf(*ll::mod::NativeMod::current()) {}
+    ClangeMeMod();
 
-bool ClangeMeMod::load() {
-    auto &self = getSelf();
-
-    self.getLogger().info("Button Test loading!");
-
-    std::error_code ec;
-
-    std::filesystem::create_directories(
-        self.getDataDir(),
-        ec
-    );
-
-    if (ec) {
-        self.getLogger().error(
-            "Failed to create data directory: {}",
-            ec.message()
-        );
-        return false;
+    [[nodiscard]] ll::mod::NativeMod &getSelf() const {
+        return mSelf;
     }
 
-    std::filesystem::create_directories(
-        self.getConfigDir(),
-        ec
-    );
+    bool load();
+    bool enable();
+    bool disable();
+    bool unload();
 
-    if (ec) {
-        self.getLogger().error(
-            "Failed to create config directory: {}",
-            ec.message()
-        );
-        return false;
-    }
+private:
+    ll::mod::NativeMod &mSelf;
 
-    return true;
-}
+    ModConfig mConfig;
 
-bool ClangeMeMod::enable() {
-    auto &self = getSelf();
-
-    self.getLogger().info(
-        "Button Test enabling!"
-    );
-
-    const bool registered =
-        pl::modmenu::ButtonBuilder(
-            "buttontest.test_button",
-            "Button Test"
-        )
-        .modId(self.getId())
-        .label("TEST")
-        .androidKeyCode(0)
-        .behavior(
-            pl::modmenu::ButtonBehavior::Click
-        )
-        .onEvent(
-            [&self](
-                std::string_view buttonId,
-                pl::modmenu::ButtonEvent event,
-                float value
-            ) {
-                (void)buttonId;
-                (void)value;
-
-                if (
-                    event ==
-                    pl::modmenu::ButtonEvent::Click
-                ) {
-                    self.getLogger().info(
-                        "BUTTON TEST CLICKED!"
-                    );
-                }
-            }
-        )
-        .registerButton();
-
-    if (!registered) {
-        self.getLogger().error(
-            "Failed to register TEST button!"
-        );
-
-        return false;
-    }
-
-    self.getLogger().info(
-        "TEST button registered!"
-    );
-
-    return true;
-}
-
-bool ClangeMeMod::disable() {
-    getSelf().getLogger().info(
-        "Button Test disabling..."
-    );
-
-    pl::modmenu::unregisterButton(
-        "buttontest.test_button"
-    );
-
-    return true;
-}
-
-bool ClangeMeMod::unload() {
-    getSelf().getLogger().info(
-        "Button Test unloaded."
-    );
-
-    return true;
-}
+    std::optional<pl::config::ConfigFile<ModConfig>> mConfigFile;
+};
 
 } // namespace clange_me
